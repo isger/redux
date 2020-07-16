249a1a82f542d236e4ac6b37924ebd5a6c439227
DarkOutlaw101 <james@isger.co.uk>
2020-07-16 11:55:40 +0100

    Multiple Counters

--- src/components/core/App.js
+++ src/components/core/App.js
@@ -1,25 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 import AddButton from "../add-button/AddButton";
 import RemoveButton from "../remove-button/RemoveButton";
 
-import { useSelector } from "react-redux";
+import {useSelector} from "react-redux";
 
 function App() {
 
     const counter = useSelector(state => state.counterReducer);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="text-6xl mx-auto my-8">{counter}</h1>
-          <div className="inline-block">
-            <RemoveButton /> <AddButton />
-          </div>
-      </header>
-    </div>
-  );
+    let [counterIndex, setCounterIndex] = useState(1);
+    let [counterArray, setCounterArray] = useState([]);
+
+    const showMoreCounters = () => {
+
+        setCounterIndex(counterIndex + 1);
+
+        for (let i = 0; i < counterIndex || i === counterIndex; i++) {
+            setCounterArray(counterArray.concat(
+                <div key={i} className="text-center w-1/3">
+                    <h1 className="text-6xl mx-auto my-8">Counter {i}</h1>
+                    <h1 className="text-6xl mx-auto my-8">{counter}</h1>
+                    <div className="inline-block">
+                        <RemoveButton/> <AddButton/>
+                    </div>
+                </div>
+            ))
+        }
+
+        console.log(counterArray);
+        return counterArray;
+
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <button
+                    className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow my-8 mx-8"
+                    onClick={() => showMoreCounters()}>Add a counter
+                </button>
+                <div className="flex">
+                    {counterArray.map((value, index) => {
+                        return value;
+                    })}
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
