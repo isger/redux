2650df9509865df89727f8a0e9a7ac3f5c96973d
DarkOutlaw101 <james@isger.co.uk>
2020-07-24 20:24:05 +0100

    Updates

--- src/components/add-counter-button/AddCounterButton.js
+++ src/components/add-counter-button/AddCounterButton.js
@@ -5,19 +5,14 @@ import {useDispatch} from "react-redux";
 
 import { addCounter } from "../../redux/actions";
 
-const initialState = {
-    counters: [{counter: 0}]
-}
-
 const AddButton = (state, action) => {
-    if (typeof state === 'undefined') {
-        return initialState
-    }
+
+    let counterID = 1;
 
     const dispatch = useDispatch();
 
     return (
-        <button onClick={() => dispatch(addCounter())}
+        <button onClick={() => dispatch(addCounter(counterID))}
             className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow my-8 mx-8">Add a counter
         </button>
     )
